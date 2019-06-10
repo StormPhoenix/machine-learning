9bcccbb44883a3fccb74d0354d0a6864451fd843
stormphoenix <stormphoenix.genius@hotmail.com>
2019-06-10 10:47:06 +0800

    add lwlr

--- linear_regression/regression.py
+++ linear_regression/regression.py
@@ -4,6 +4,29 @@ import graph.scatter as scatter
 import numpy as np
 import numpy.linalg as linalg
 
+'''
+TO DO LIST 仔细学习矩阵求导
+'''
+
+
+def locallyWeightedLinearRegres(predictX, dataArr, labelArr, k=0.01):
+    xMat = np.mat(dataArr)
+    yMat = np.mat(labelArr).transpose()
+    m, _ = np.shape(xMat)
+    weights = np.mat(np.eye(m))
+
+    for j in range(m):
+        diffMat = predictX - xMat[j, :]
+        weights[j, j] = np.exp(np.dot(diffMat, diffMat.T) / (-2.0 * k ** 2))
+
+    xTWx = xMat.T * (weights * xMat)
+    if linalg.det(xTWx) == 0:
+        print('This matrix is singular, cannot do inverse')
+        return
+
+    ws = xTWx.I * (xMat.T * (weights * yMat))
+    return predictX * ws
+
 
 def standRegres(xArr, yArr):
     '''
@@ -26,7 +49,7 @@ def standRegres(xArr, yArr):
     return ws
 
 
-def main():
+def testLinearRegres():
     dataArr, labelArr = loader.loadDataSet()
     ws = standRegres(dataArr, labelArr)
     print('ws:', ws)
@@ -35,6 +58,7 @@ def main():
     yMat = np.mat(labelArr).transpose()
     yHat = np.dot(xMat, ws)
 
+    # draw graph
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(xMat[:, 1].flatten().A1, yMat[:, 0].flatten().A1)
@@ -46,6 +70,34 @@ def main():
     coef = np.corrcoef(yHat.T, yMat.T)
     print("coef shape", np.shape(coef))
     print("coef: ", coef)
+
+
+def testLwlr():
+    dataArr, labelArr = loader.loadDataSet()
+    dataMat = np.mat(dataArr)
+    labelMat = np.mat(labelArr).transpose()
+    m, _ = np.shape(dataMat)
+
+    xMat = dataMat.copy()
+    xMat = xMat[xMat[:, 1].argsort(0)][:, 0, :]
+    yHat = np.mat(np.zeros((m, 1)))
+    for i in range(m):
+        yHat[i] = locallyWeightedLinearRegres(xMat[i, :], dataArr, labelArr)
+    # draw graph
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(dataMat[:, 1].flatten().A1, labelMat[:, 0].flatten().A1)
+    ax.plot(xMat[:, 1].flatten().A1, yHat[:, 0].flatten().A1)
+    plt.show()
+    # 计算相关系数
+    coef = np.corrcoef(yHat.T, labelMat.T)
+    print("coef shape", np.shape(coef))
+    print("coef: ", coef)
+
+
+def main():
+    # testLinearRegres()
+    testLwlr()
 
 
 if __name__ == '__main__':
